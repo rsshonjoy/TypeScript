264c2e70fabb0ad4d60d36e94ddc8e4b609c9947
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-22 17:25:50 +0600

    Type Aliases

--- dist/typeAliases.js
+++ dist/typeAliases.js
@@ -1,4 +1,10 @@
 "use strict";
+// const userDetails = (
+//   id: string | number,
+//   user: { name: string, age: number}
+// ) => {
+//   console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+// };
 const userDetails = (id, user) => {
     console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
 };
--- typeAliases.ts
+++ typeAliases.ts
@@ -1,10 +1,23 @@
-const userDetails = (
-  id: string | number,
-  user: { name: string, age: number}
-) => {
+// const userDetails = (
+//   id: string | number,
+//   user: { name: string, age: number}
+// ) => {
+//   console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+// };
+
+// const sayHello = (user: { name : string; age: number}) => {
+//   console.log(`Hello ${user.age > 50 ? "sir": "Mr."} ${user.name}`);
+// }
+
+
+
+type idType = string | number;
+type userType = { name: string, age: number}
+
+const userDetails = (id: idType, user: userType) => {
   console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
 };
 
-const sayHello = (user: { name : string; age: number}) => {
+const sayHello = (user: userType) => {
   console.log(`Hello ${user.age > 50 ? "sir": "Mr."} ${user.name}`);
 }
