600867d7544d98bca5391249846f9e7cd1725307
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-22 18:05:04 +0600

    class

---
+++ class.ts
@@ -0,0 +1,18 @@
+class Player {
+  name: string;
+  age: number;
+  country: string;
+
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
+  }
+
+  play(){
+    console.log(`${this.name} from ${this.country} is playing!`);
+  }
+}
+
+const mashrafi = new Player('Mashrafi', 40, 'Bangladesh')
+console.log(mashrafi);
---
+++ dist/class.js
@@ -0,0 +1,13 @@
+"use strict";
+class Player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country} is playing!`);
+    }
+}
+const mashrafi = new Player('Mashrafi', 40, 'Bangladesh');
+console.log(mashrafi);
