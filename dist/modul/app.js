b9dd7a906567108a3932b3341d5737d2a395a175
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-22 20:54:13 +0600

    modul

---
+++ dist/modul/app.js
@@ -0,0 +1,9 @@
+import { Player2 } from './classes/Player.js';
+const mashrafi2 = new Player2('Mashrafi', 40, 'Bangladesh');
+// console.log(mashrafi);
+const sakib2 = new Player2('Sakib', 36, 'Bangladesh');
+// console.log(sakib);
+const players2 = [];
+players2.push(sakib2);
+players2.push(mashrafi2);
+console.log(players2);
