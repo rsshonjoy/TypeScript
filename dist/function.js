2dd3df2be851f38efecfda4b59035ca5f5c5c93c
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-22 16:45:22 +0600

    function

---
+++ dist/function.js
@@ -0,0 +1,6 @@
+"use strict";
+/** ----- Step 1 ----- */
+const myFunction = () => {
+    console.log("Hello");
+};
+myFunction();
---
+++ function.ts
@@ -0,0 +1,7 @@
+/** ----- Step 1 ----- */
+
+const myFunction = () => {
+  console.log("Hello");
+}
+myFunction()
+
