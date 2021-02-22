8d9268234bfe288c2cba4e8fbc36ba5ed0b4fc45
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-22 22:00:43 +0600

    tuples

---
+++ dist/tuples.js
@@ -0,0 +1,4 @@
+"use strict";
+let a = [7, 'world', { a: 3 }];
+let b = [4, 'world', { t: 2 }];
+console.log(b);
---
+++ tuples.ts
@@ -0,0 +1,5 @@
+let a = [7, 'world', {a: 3}]
+
+let b:[number, string, object] = [4, 'world', {t: 2}]
+
+console.log(b);
