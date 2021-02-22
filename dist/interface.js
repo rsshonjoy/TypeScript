5787ddebb6a9d3a15a80f4e9d256d5e05fb30ee9
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-22 21:27:19 +0600

    interface

---
+++ dist/interface.js
@@ -0,0 +1,11 @@
+"use strict";
+function drawRectangle(options) {
+    let width = options.width;
+    let length = options.length;
+}
+let threeDdOptions = {
+    width: 30,
+    length: 20,
+    height: 10,
+};
+drawRectangle(threeDdOptions);
---
+++ interface.ts
@@ -0,0 +1,18 @@
+interface RectangleOptions {
+  width: number;
+  length: number;
+}
+
+function drawRectangle(options: RectangleOptions) {
+  let width = options.width;
+  let length = options.length;
+}
+
+let threeDdOptions = {
+  width: 30,
+  length: 20,
+  height: 10,
+}
+
+drawRectangle(threeDdOptions)
+
